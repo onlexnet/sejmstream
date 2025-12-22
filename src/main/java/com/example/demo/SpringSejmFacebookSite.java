790f8a31d99c289e326e97e631943a27e139e978
alemisiu <alek@siudek.net>
2025-12-22 18:35:51 +0100

    Initialize Spring Boot project with basic structure and dependencies

---
+++ src/main/java/com/example/demo/SpringSejmFacebookSite.java
@@ -0,0 +1,52 @@
+package com.example.demo;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+import com.restfb.Connection;
+import com.restfb.DefaultFacebookClient;
+import com.restfb.Version;
+import com.restfb.types.Page;
+import com.restfb.types.Post;
+
+@Component
+public class SpringSejmFacebookSite implements  CommandLineRunner {
+    
+    @Value("${FB_TOKEN}")
+    String fbToken;
+
+   @Override
+    public void run(String... args) throws Exception {
+        var fbClient = new DefaultFacebookClient(fbToken, Version.LATEST);
+        Connection<Page> pages = fbClient.fetchConnection("me/accounts", Page.class);
+
+        var pat = pages.getData().stream()
+                .filter(it -> it.getName().equals("SejmStream2"))
+                .findFirst()
+                .get()
+                .getAccessToken();
+
+        var pageClient = new DefaultFacebookClient(pat, Version.LATEST);
+        var feed = pageClient.fetchConnection("me/feed", Post.class);
+        for(var post : feed.getData()) {
+            System.out.println("Post: " + post.getMessage());
+        }
+
+        // generate a simple image and publish in the feed
+        var imageUrl = "https://www.yttags.com/blog/wp-content/uploads/2023/02/image-urls-for-testing.webp";
+        var publishPhoto = pageClient.publish("me/photos", com.restfb.types.FacebookType.class,
+                com.restfb.Parameter.with("url", imageUrl),
+                com.restfb.Parameter.with("caption", "Hello from RestFB!"),
+                com.restfb.Parameter.with("is_published", false));
+        System.out.println("Published photo ID: " + publishPhoto.getId());
+
+        // create a post
+        var publishPost = pageClient.publish("me/feed", com.restfb.types.FacebookType.class,
+                com.restfb.Parameter.with("message", "Hello from RestFB!"),
+                com.restfb.Parameter.with("is_published", false));
+        System.out.println("Published post ID: " + publishPost.getId());
+    }
+
+   
+}
