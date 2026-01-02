662717fabcf6cc0911a0ff40a9c5b554247ac958
alemisiu <alek@siudek.net>
2026-01-02 16:37:57 +0100

    Add Vote record and update VotingStats to include votes list

--- src/main/java/com/example/demo/Models.java
+++ src/main/java/com/example/demo/Models.java
@@ -1,12 +1,16 @@
 package com.example.demo;
 
 import java.time.LocalDate;
+import java.util.List;
 
 // https://api.sejm.gov.pl/sejm/term10/MP/1
 record MP(String firstLastName, int id, String club, boolean active) {
 }
 
 record Term(boolean current, LocalDate from, int num, LocalDate to) {
+}
+
+record Vote(int mpId, boolean present) {
 }
 
 record VotingStats(
@@ -21,6 +25,9 @@ record VotingStats(
         // liczba oddanych głosów
         int numVoted,
         // numer posiedzenia
-        int sitting
+        int sitting,
+        //lista głosów
+        List<Vote> votes
 ) {
 }
+
