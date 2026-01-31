246cfb4849a5bfb2bbdc38862505fac3dfc8426b
alemisiu <alek@siudek.net>
2026-01-31 14:44:24 +0100

    Refactor SejmApiImpl: improve code formatting and enhance getVotingStats method

--- src/main/java/com/example/demo/SejmApi.java
+++ src/main/java/com/example/demo/SejmApi.java
@@ -9,39 +9,55 @@ import org.springframework.web.client.RestClient;
 
 public interface SejmApi {
     List<Term> getTerms();
-    
-    List<MP> getMPs(int termNumber);
-    
-    List<VotingStats> getVotingStats(int termNumber, int mpId);
-}
 
+    List<MP> getMPs(int termNumber);
+
+    List<VotingStats> getVotingStats(int termNumber, int mpId);
+
+}
 
 @Component
 class SejmApiImpl implements SejmApi {
 
     private final RestClient restClient = RestClient.create("https://api.sejm.gov.pl");
 
+    private final VotingStatsDbRepository votingStatsRepo;
+
+    SejmApiImpl(VotingStatsDbRepository votingStatsRepo) {
+        this.votingStatsRepo = votingStatsRepo;
+    }
+
     @Override
     @Cacheable("terms")
     public List<Term> getTerms() {
-        var termType = new ParameterizedTypeReference<List<Term>>() {};
+        var termType = new ParameterizedTypeReference<List<Term>>() {
+        };
         return restClient.get().uri("sejm/term").retrieve().body(termType);
     }
 
     @Override
     @Cacheable("mps")
     public List<MP> getMPs(int termNumber) {
-        var type = new ParameterizedTypeReference<List<MP>>() {};
+        var type = new ParameterizedTypeReference<List<MP>>() {
+        };
         return restClient.get().uri("sejm/term{termNo}/MP", termNumber).retrieve().body(type);
     }
 
     @Override
     @Cacheable("votingStats")
     public List<VotingStats> getVotingStats(int termNumber, int mpId) {
-        var type = new ParameterizedTypeReference<List<VotingStats>>() {};
-        return restClient.get()
-            .uri("sejm/term{termNo}/MP/{mpId}/votings/stats", termNumber, mpId)
-            .retrieve()
-            .body(type);
+        var type = new ParameterizedTypeReference<List<VotingStats>>() {
+        };
+        var result = restClient.get()
+                .uri("sejm/term{termNo}/MP/{mpId}/votings/stats", termNumber, mpId)
+                .retrieve()
+                .body(type);
+
+        var resultDbo = result.stream().map(it -> {
+            return new VotingStatsDb(it.absenceExcuse(), it.date(), it.numMissed(), it.numVotings(), it.numVoted(),
+                    it.sitting());
+        }).toList();
+        votingStatsRepo.saveAll(resultDbo);
+        return result;
     }
 }
