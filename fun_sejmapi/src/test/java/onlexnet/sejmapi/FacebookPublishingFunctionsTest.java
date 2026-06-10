b3479eea805994e1597df8d58994b593d1c38b46
siudeks <slawomir@siudek.net>
2026-06-10 11:58:56 +0000

    test: update timer trigger test to reflect daily schedule at 6AM

--- fun_sejmapi/src/test/java/onlexnet/sejmapi/FacebookPublishingFunctionsTest.java
+++ fun_sejmapi/src/test/java/onlexnet/sejmapi/FacebookPublishingFunctionsTest.java
@@ -13,7 +13,7 @@ import com.microsoft.azure.functions.annotation.TimerTrigger;
 class FacebookPublishingFunctionsTest {
 
     @Test
-    void givenFacebookPublishFunction_whenInspectingTimerTrigger_thenRunsEveryTenMinutes()
+    void givenFacebookPublishFunction_whenInspectingTimerTrigger_thenRunsDailyAt6AM()
             throws NoSuchMethodException {
         var method = FacebookPublishingFunctions.class.getDeclaredMethod(
                 "publishHelloMessage",
@@ -26,7 +26,7 @@ class FacebookPublishingFunctionsTest {
         assertThat(functionName).isNotNull();
         assertThat(functionName.value()).isEqualTo("SejmApiDemo_FacebookPublish");
         assertThat(trigger).isNotNull();
-        assertThat(trigger.schedule()).isEqualTo("0 */10 * * * *");
+        assertThat(trigger.schedule()).isEqualTo("0 0 6 * * *");
     }
 
     @Test
