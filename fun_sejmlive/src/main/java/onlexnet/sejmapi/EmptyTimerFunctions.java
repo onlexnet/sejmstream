47e73ca680b8d40867cee72e6df2f25f0f6b3b8f
siudeks <slawomir@siudek.net>
2026-06-11 12:17:24 +0000

    refactor: remove EveryTenMinutesTimerTrigger annotation and use TimerTrigger directly

--- fun_sejmlive/src/main/java/onlexnet/sejmapi/EmptyTimerFunctions.java
+++ fun_sejmlive/src/main/java/onlexnet/sejmapi/EmptyTimerFunctions.java
@@ -4,10 +4,10 @@ import org.springframework.stereotype.Component;
 
 import com.microsoft.azure.functions.ExecutionContext;
 import com.microsoft.azure.functions.annotation.FunctionName;
+import com.microsoft.azure.functions.annotation.TimerTrigger;
 
 import liquibase.integration.spring.SpringLiquibase;
 import lombok.RequiredArgsConstructor;
-import onlexnet.infra.adapters.in.schedule.EveryTenMinutesTimerTrigger;
 
 /**
  * Empty timer-triggered function used to measure how quickly the host executes
@@ -21,7 +21,7 @@ public final class EmptyTimerFunctions {
 
     @FunctionName("SejmApiDemo_EmptyTimer")
     public void runEmptyTimer(
-            @EveryTenMinutesTimerTrigger
+            @TimerTrigger(name = "timer", schedule = "0 */10 * * * *")
             final String timerInfo,
             final ExecutionContext executionContext) {
 
