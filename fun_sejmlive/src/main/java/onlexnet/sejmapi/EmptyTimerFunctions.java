a787c9f5b6236baf6b6b20d4638e34e36121a417
siudeks <slawomir@siudek.net>
2026-06-11 07:21:37 +0000

    refactor: replace TimerTrigger annotation with custom EveryTenMinutesTimerTrigger

--- fun_sejmlive/src/main/java/onlexnet/sejmapi/EmptyTimerFunctions.java
+++ fun_sejmlive/src/main/java/onlexnet/sejmapi/EmptyTimerFunctions.java
@@ -2,7 +2,7 @@ package onlexnet.sejmapi;
 
 import com.microsoft.azure.functions.ExecutionContext;
 import com.microsoft.azure.functions.annotation.FunctionName;
-import com.microsoft.azure.functions.annotation.TimerTrigger;
+import onlexnet.infra.adapters.in.schedule.EveryTenMinutesTimerTrigger;
 
 /**
  * Empty timer-triggered function used to measure how quickly the host executes
@@ -12,7 +12,7 @@ public final class EmptyTimerFunctions {
 
     @FunctionName("SejmApiDemo_EmptyTimer")
     public void runEmptyTimer(
-            @TimerTrigger(name = "timer", schedule = "0 */10 * * * *")
+            @EveryTenMinutesTimerTrigger
             final String timerInfo,
             final ExecutionContext executionContext) {
 
