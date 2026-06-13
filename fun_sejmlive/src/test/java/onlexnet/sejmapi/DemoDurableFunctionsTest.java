44fe41e7b3a48955758167fd737bdd3674fb41be
siudeks <slawomir@siudek.net>
2026-06-13 23:08:38 +0000

    feat: add HTTP trigger for publishing daily digest to Facebook and update OpenAPI documentation

--- fun_sejmlive/src/test/java/onlexnet/sejmapi/DemoDurableFunctionsTest.java
+++ fun_sejmlive/src/test/java/onlexnet/sejmapi/DemoDurableFunctionsTest.java
@@ -80,6 +80,8 @@ class DemoDurableFunctionsTest {
     assertThat(response.getBody().toString())
         .contains("openapi: 3.0.3")
         .contains("/api/SejmApiDemo_HttpStart")
+        .contains("/api/SejmApiDemo_CollectStart")
+        .contains("/api/SejmApiDemo_FacebookPublishStart")
         .contains("/api/sejm-api/simple")
         .contains("Sejm API simple data")
         .contains("functionKey")
